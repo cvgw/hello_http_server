dd750e4b5ee02ac29cce74e87b92804179f6b02f
Cole Wippern <cgwippern@gmail.com>
2018-08-29 22:37:53 -0700

    Parse buffer

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int getAddressInfo(char host[], char port[], struct addrinfo **res)
 {
@@ -26,8 +27,89 @@ int getAddressInfo(char host[], char port[], struct addrinfo **res)
     return 0;
 }
 
+void processBuffer(char *buffer)
+{
+    int currentBuffLen, i = 0;
+    char *operation, *header, **headers;
+    const char *currentBuff;
+    const char *nextBuff;
+
+    currentBuff = buffer;
+
+    nextBuff = strchr(currentBuff, '\n');
+    currentBuffLen = (nextBuff - currentBuff);
+    operation = malloc(currentBuffLen);
+    if (operation)
+    {
+        memcpy(operation, currentBuff, currentBuffLen);
+        printf("Operation: %s\n", operation);
+    }
+    else
+    {
+        perror("malloc");
+        exit(1);
+    }
+
+    currentBuff = nextBuff + 1;
+
+    headers = malloc(sizeof(char *) * 10);
+    if (headers == NULL)
+    {
+        perror("malloc");
+        exit(1);
+    }
+    i = 0;
+
+    while (currentBuff)
+    {
+        nextBuff = strchr(currentBuff, '\n');
+        currentBuffLen = (nextBuff - currentBuff);
+        header = malloc(currentBuffLen + 1);
+        if (header)
+        {
+            memcpy(header, currentBuff, currentBuffLen);
+            header[currentBuffLen] = '\0';
+            if (strcmp(header, "\r") == 0)
+            {
+                break;
+            }
+            printf("Header %d: %s\n", i, header);
+            *(headers + i) = header;
+        }
+        else
+        {
+            perror("malloc");
+            exit(1);
+        }
+        currentBuff = nextBuff + 1;
+        i++;
+        free(header);
+    }
+
+    currentBuff = nextBuff + 1;
+
+    int buffLen = strlen(currentBuff);
+    char *body = malloc(buffLen + 1);
+    if (body)
+    {
+        memcpy(body, currentBuff, buffLen);
+        body[buffLen] = '\0';
+        printf("Body: %s\n", body);
+    }
+    else
+    {
+        perror("malloc");
+        exit(1);
+    }
+
+    free(operation);
+    free(header);
+    free(headers);
+    free(body);
+}
+
 int main() {
-    int socketfd, numBytesRecv, numBytesSent, bufferSize = 6000;
+    int socketfd, session_fd, numBytesRecv, bufferSize = 6000;
     char buffer[bufferSize], port[] = "8081", host[] = "localhost";
     struct addrinfo *res;
     struct sockaddr_in *socketaddr;
@@ -35,6 +117,10 @@ int main() {
     getAddressInfo(host, port, &res);
 
     socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketfd == -1) {
+        perror("socket");
+        exit(1);
+    }
 
     socketaddr = (struct sockaddr_in *)res->ai_addr;
     
@@ -50,7 +136,7 @@ int main() {
     }
     printf("Listen success\n");
 
-    int session_fd = accept(socketfd, 0, 0);
+    session_fd = accept(socketfd, 0, 0);
     if (session_fd == -1)
     {
         perror("accept");
@@ -63,12 +149,16 @@ int main() {
         exit(1);
     }
 
-    printf("Buffer:\n%s\n", buffer);
+    processBuffer(buffer);
 
-    numBytesSent = send(session_fd, &buffer, bufferSize, 0);
-    if ( numBytesSent == -1 ) {
-        perror("send");
-        exit(1);
+    if (close(session_fd) != 0)
+    {
+        perror("close");
+    }
+    
+    if (close(socketfd) != 0)
+    {
+        perror("close");
     }
 
     return 0;
